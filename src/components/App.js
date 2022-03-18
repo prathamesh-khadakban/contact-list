5bc6f0cf334cf3e2ded2babc5e1492aed8c40e6d
prathamesh <prathamesh.khadkban@gmail.com>
2022-03-18 13:03:39 +0530

    store data in localstorgae

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import AddContact from "./AddContact";
 import Header from "./Header";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ContactList from "./ContactList";
 //import { uuid } from "uuidv4";
 
 function App() {
   //const dummy_contact = [{}];
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     const mergeData = [...contacts, { id: Math.random(), ...contact }];
@@ -17,6 +18,15 @@ function App() {
     });
     setContacts(filteredContacts);
   };
+  // add dependancy params cause on contacts chnage that should also update in local storage
+  useEffect(() => {
+    const retiveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retiveContacts) setContacts(retiveContacts);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div>
       <Header></Header>
